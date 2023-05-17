040417bfe5a711b7f7369519adf242d9625b604b
Rodrigo <45924807+nepht022@users.noreply.github.com>
2023-05-17 14:40:05 -0300

    Update processa_envio.php

--- processa_envio.php
+++ processa_envio.php
@@ -41,12 +41,12 @@
         //Server settings
         $mail->SMTPDebug = false;                      //Enable verbose debug output
         $mail->isSMTP();                                            //Send using SMTP
-        $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+        $mail->Host       = 'host server';                     //Set the SMTP server to send through
         $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
         $mail->Username   = 'email';                     //SMTP username
-        $mail->Password   = 'kmlcxgdhdghsurur';                               //SMTP password
+        $mail->Password   = 'senha';                               //SMTP password
         $mail->SMTPSecure = 'tls';            //Enable implicit TLS encryption
-        $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+        $mail->Port       = 'porta';                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
         //Recipients
         //quem vai enviar e receber o email
